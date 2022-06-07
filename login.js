8042ebcd95eb19a971ae9188d091fbeea68d1568
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-06-07 17:30:38 +0200

    login button

--- login.js
+++ login.js
@@ -2,16 +2,23 @@ async function renderAccount(){
     let url = './account.json';
     let response = await fetch(url);
     let account = await response.json();
+    login(account)
     
-    login(account);
+    
 }
 
 function login(account){
- /*   document.getElementById('container').style.display = "none"; */
-    body.innerHTML += `
+    let username = document.getElementById('username').value;
+    let userpassword = document.getElementById('userpassword').value;
     
-        <div class="login">
-        </div>
-    `;
-    console.log(account[1].name);
+    for(let i = 0; i <account.length; i++){
+        if(account[i].name == username && account[i].password == userpassword){
+            document.getElementById('loginContainer').classList.add('display_none');
+
+        }
+    
+
+    }
+
+
 }
