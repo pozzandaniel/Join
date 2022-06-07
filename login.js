0d189e2690b92519e3157cee9fd643da88a6f468
Norbert Madarász <norbertmadarasz@Norbert-MBP.home>
2022-06-07 18:01:57 +0200

    login function

--- login.js
+++ login.js
@@ -1,20 +1,27 @@
+
+
 async function renderAccount(){
     let url = './account.json';
     let response = await fetch(url);
-    let account = await response.json();
-    login(account)
+    account = await response.json();
     
+    login(account);
     
 }
 
 function login(account){
-    let username = document.getElementById('username').value;
-    let userpassword = document.getElementById('userpassword').value;
-    
-    for(let i = 0; i <account.length; i++){
-        if(account[i].name == username && account[i].password == userpassword){
-            document.getElementById('loginContainer').classList.add('display_none');
 
+        //    document.getElementById('loginContainer').classList.add('display_none');
+    let username = document.getElementById('username');
+    let userpassword = document.getElementById('userpassword');
+    
+    for(let i = 0; i < account.length; i++){
+
+
+        if(account[i].name == username.value && account[i].password == userpassword.value){
+
+
+            
         }
     
 
